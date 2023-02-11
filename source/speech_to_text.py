d19295daaa9f5661c19b990e488c0180551dc678
Michal <mjanuszewski57@gmail.com>
2023-02-11 19:25:57 +0100

    speech-to-text module

--- source/speech_to_text.py
+++ source/speech_to_text.py
@@ -1,4 +1,27 @@
-from pocketsphinx import LiveSpeech
+import speech_recognition as sr
+import time 
 
-for phrase in LiveSpeech():
-    print(phrase)
+r = sr.Recognizer()
+r.operation_timeout = 1.8
+mic = sr.Microphone()
+
+
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+
+out=""
+def callback(r, audio):
+    try: 
+        # with sr.Microphone() as source:
+            # print("Słucham")
+            # r.adjust_for_ambient_noise(source)
+            # audio = r.listen(source)
+            # print("analizuję")
+        out = r.recognize_google(audio)
+        print(out)
+    except sr.UnknownValueError: 
+        print("Jeszcze raz, nie dosłyszałem.")
+ 
+stop_listening = r.listen_in_background(mic, callback, phrase_time_limit=3)
+
+time.sleep(100)
